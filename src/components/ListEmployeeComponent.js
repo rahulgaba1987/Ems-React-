481949140aede01bf9747da1cf64523c21765c5e
Rahul <Rahulgaba4u1987@gmail.com>
2024-02-01 10:22:31 +0530

    Employee delete operation

--- src/components/ListEmployeeComponent.js
+++ src/components/ListEmployeeComponent.js
@@ -1,34 +1,45 @@
-import React,{useEffect,useState} from 'react'
-import listEmployees from '../services/EmployeeService';
+import React, { useEffect, useState } from 'react'
+import listEmployees, { deleteEmployeeById } from '../services/EmployeeService';
 import { useNavigate } from 'react-router-dom';
 
 
 const ListEmployeeComponent = () => {
-    
-      const [employees,setEmployees]= useState([]);
-      const navigator= useNavigate();
 
-      useEffect(()=>
-      {
-    
-           listEmployees().then((response)=>
-           {
-                setEmployees(response.data);
-           }).catch(
-            (error)=>console.error(error));
-           
+    const [employees, setEmployees] = useState([]);
+    const navigator = useNavigate();
 
-      },[]);
+    useEffect(() => {
 
-      function updateEmployee(employeeId)
-      {
+        getAllEmployees();
+
+
+    }, []);
+
+    function getAllEmployees() {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(
+            (error) => console.error(error));
+    }
+
+    function updateEmployee(employeeId) {
         navigator(`/edit-employee/${employeeId}`);
-      }
+    }
 
-      function addEmployee()
-      {
-           navigator(`/add-employee`);
-      }
+    function addEmployee() {
+        navigator(`/add-employee`);
+    }
+
+    function removeEmployee(id) {
+       
+        deleteEmployeeById(id).then((response) => {
+
+            getAllEmployees();
+
+        }).catch(error => {
+            console.error(error);
+        })
+    }
     return (
         <div className='container'>
             <h1 className='text-center'>ListEmployeeComponent</h1>
@@ -45,15 +56,15 @@ const ListEmployeeComponent = () => {
                 </thead>
                 <tbody>
                     {
-                        employees.map(emp => 
+                        employees.map(emp =>
                             <tr key={emp.employeeId}>
                                 <td>{emp.employeeId}</td>
                                 <td>{emp.firstName}</td>
                                 <td>{emp.lastName}</td>
                                 <td>{emp.email}</td>
                                 <td>
-                                    <button className='btn btn-info' onClick={()=>updateEmployee(emp.employeeId)}>Update</button> &nbsp;&nbsp;
-                                    <button className='btn btn-danger'>Delete</button>
+                                    <button className='btn btn-info' onClick={() => updateEmployee(emp.employeeId)}>Update</button> &nbsp;&nbsp;
+                                    <button className='btn btn-danger' onClick={() => removeEmployee(emp.employeeId)}>Delete</button>
                                 </td>
                             </tr>
 
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -10,5 +10,7 @@ const REST_API_BASE_URL ='http://localhost:8080/api/employees/';
 
  export const updateEmployeeById=(employeeId,employee)=> axios.put(REST_API_BASE_URL+employeeId,employee);
 
+ export const deleteEmployeeById=(employeeId)=> axios.get(REST_API_BASE_URL+employeeId);
+
 
 export default listEmployees;
